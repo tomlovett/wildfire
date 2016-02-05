65358216c57fe84996f25c6bd1dc1f739bd9b400
Tom Lovett <tomlovett6@gmail.com>
2016-02-04 17:45:41 -0700

    prototype built, now need to process

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
 angular.module('app', [])
   .controller('MainController', ['$scope', function($scope) {
 
-    $scope.lake = "'background-color' : 'blue'"
-    $scope.ignited = "'background-color' : 'orange'"
-    $scope.burned = "'background-color' : 'red'"
-
     $scope.ignite = function(index) {
         $scope.land[index]
     }
 
-    $scope.Tile = function(land, index) {
+    var Tile = function(land, tileList, index) {
         // style = initateBurn -> orange, burnNeighbors -> red 
-        this.lake = false
         this.land = land
-        this.style = "'background-color' :'white'"
+        this.tileList = tileList
         this.index = index
+        this.style = "'background-color' :'white'"
+        this.lake = false
     }
 
-    $scope.Tile.prototype = {
+    Tile.prototype = {
         ignite : function(tileList) {
             if (this.land == 'Lake') { return }
-            console.lop('clicked this guy: ', this)
+            console.log('clicked this guy: ', this)
             this.style = "'background-color' : 'orange'"
-            setTimeout(this.spreadToNeighbors(tileList, this.ignite, 500))
+            setTimeout(this.igniteNeighbors, 500)
         },
         burn : function(tileList) {
             this.style = "'background-color' : 'red'"
             setTimeout(this.burnNeighbors, 1000)
         },
-        spreadToNeighbors : function(tileList, func) {
+        igniteNeighbors : function(tileList) {  // way to pass ignite or burn as functions to cut down double-code?
             neighborA = this.findNeighbor(+1)
             neighborB = this.findNeighbor(-1)
-            neighborA.func()
-            neighborB.func()
+            neighborA.ignite()
+            neighborB.ignite()
         },
         burnNeighbors : function(tileList) {
-            return
+            neighborA = this.findNeighbor(+1)
+            neighborB = this.findNeighbor(-1)
+            neighborA.burn()
+            neighborB.burn()
         },
         findNeighbors : function(tileList, shift) {
             var a = (this.index + shift) % tile.list.length
             return tileList[index]
         }
     }
+
+    // processLands
 
 // initiateBurn -> on-click
 // burnNeighbors -> timeout 
